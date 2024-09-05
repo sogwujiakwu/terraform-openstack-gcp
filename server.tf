d8d601c5d18ae1f0a63514d6246713ef90082e06
sogwujiakwu <sogwujiakwu@gmail.com>
2024-09-05 12:36:13 +0000

    added server.tf and vpc.tf. deleted main.tf

--- server.tf
+++ server.tf
@@ -1,23 +1,8 @@
-/*terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.1.0"
-    }
-  }
-}
-
-provider "google" {
-  project = "smart-nomad-433514-k5"
-}
-*/
-resource "google_compute_network" "openstack_vpc_network" {
-  name = "openstack-network"
-}
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "n1-standard-4"
   zone         = "us-central1-b"
+  tags         = ["web", "dev"]
   boot_disk {
     initialize_params {
       image = "nested-vm-image"
---
+++ vpc.tf
@@ -0,0 +1,3 @@
+resource "google_compute_network" "openstack_vpc_network" {
+  name = "openstack-network"
+}
