716584eec5dc8ba72c109125396508c864cfc9a2
sogwujiakwu <sogwujiakwu@gmail.com>
2024-09-06 14:53:13 +0000

    updated server.tf, server_tf_template and added inventory

--- server.tf
+++ server.tf
@@ -1,5 +1,5 @@
 variable "zone" {
-  default = "us-east1-c"
+  default = "us-east1-b"
 }
 
 resource "google_compute_instance" "openstack_controller" {
@@ -23,6 +23,25 @@ resource "google_compute_instance" "openstack_controller" {
 
 resource "google_compute_instance" "openstack_compute" {
   name         = "openstack-compute"
+  machine_type = "n1-standard-4"
+  zone         = var.zone
+  tags         = ["http-server", "https-server", "novnc", "openstack-apis"]
+  boot_disk {
+    initialize_params {
+      size = 200
+      image = "nested-vm-image"
+    }
+  }
+  can_ip_forward = true
+  network_interface {
+    network = google_compute_network.openstack_vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "openstack_workstation" {
+  name         = "openstack-workstation"
   machine_type = "n1-standard-4"
   zone         = var.zone
   tags         = ["http-server", "https-server", "novnc", "openstack-apis"]
